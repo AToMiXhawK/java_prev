f88dea29105a92ffc47bc4ba079a15039d23c864
kryptiksage <kryptiksage@gmail.com>
2019-03-22 13:29:53 +0530

    java pkg program

---
+++ Hybrid.java
@@ -0,0 +1,31 @@
+import studpack.*;
+
+class Hybrid
+{
+    public static void main(String[] args) 
+    {
+        Result s1 = new Result("A", 1, 10, 10, 10);
+        Result s2 = new Result("A", 1, 10, 10, 10);
+        Result s3 = new Result("A", 1, 10, 10, 10);
+        Result s4 = new Result("A", 1, 10, 10, 10);
+        Result s5 = new Result("A", 1, 10, 10, 10);
+        
+        s1.display_grade();
+        s1.displayTotal();
+
+        s2.display_grade();
+        s2.displayTotal();
+        
+        s3.display_grade();
+        s3.displayTotal();
+        
+        s4.display_grade();
+        s4.displayTotal();
+        
+        s5.display_grade();
+        s5.displayTotal();
+    }
+}
+
+
+
---
+++ studpack/Result.java
@@ -0,0 +1,50 @@
+package studpack;
+
+class Student
+{
+	String name;
+	int rollno;
+	double mark1, mark2, mark3;
+	Student()
+	{
+		name="";
+		rollno=0;
+		mark1=0;
+		mark2=0;
+		mark3=0;
+	}
+	Student(String n, int r, double m1, double m2, double m3)
+	{
+		name=n;
+		rollno=r;
+		mark1=m1;	
+		mark2=m2;
+		mark3=m3;
+	}
+}
+
+interface Sports
+{
+        double grade=10;
+        void display_grade();
+}
+
+public class Result extends Student implements Sports
+{
+	double total;
+	public Result(String n, int r, double m1, double m2, double m3)
+	{
+		super(n,r,m1,m2,m3);
+		total=0;
+	}
+	public void display_grade()
+	{
+		System.out.println("Grade: "+grade);
+	}
+	public void displayTotal()
+	{
+		total = mark1 + mark2 + mark3 + grade;
+		System.out.println("Total: "+total);
+		System.out.println("Roll number: "+ rollno);
+	}
+}
